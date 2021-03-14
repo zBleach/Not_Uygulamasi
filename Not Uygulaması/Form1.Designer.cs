d7db5c3ac7f45aa11ffaf6c20ca747cabe20bf8e
zBleach <79703662+zBleach@users.noreply.github.com>
2021-03-14 16:12:44 +0300

    Initial commit

---
+++ Not Uygulaması/Form1.Designer.cs
@@ -0,0 +1,322 @@
+﻿
+namespace Not_Uygulaması
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tboxad = new System.Windows.Forms.TextBox();
+            this.tboxsoyad = new System.Windows.Forms.TextBox();
+            this.tbox1not = new System.Windows.Forms.TextBox();
+            this.tbox2not = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listboxad = new System.Windows.Forms.ListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listboxsoyad = new System.Windows.Forms.ListBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.listboxnot1 = new System.Windows.Forms.ListBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.listboxnot2 = new System.Windows.Forms.ListBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.ortalama = new System.Windows.Forms.ListBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.sonuc = new System.Windows.Forms.ListBox();
+            this.hesapla = new System.Windows.Forms.Button();
+            this.temizle = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tboxad
+            // 
+            this.tboxad.Location = new System.Drawing.Point(110, 14);
+            this.tboxad.Name = "tboxad";
+            this.tboxad.Size = new System.Drawing.Size(100, 23);
+            this.tboxad.TabIndex = 0;
+            // 
+            // tboxsoyad
+            // 
+            this.tboxsoyad.Location = new System.Drawing.Point(110, 60);
+            this.tboxsoyad.Name = "tboxsoyad";
+            this.tboxsoyad.Size = new System.Drawing.Size(100, 23);
+            this.tboxsoyad.TabIndex = 1;
+            // 
+            // tbox1not
+            // 
+            this.tbox1not.Location = new System.Drawing.Point(110, 106);
+            this.tbox1not.Name = "tbox1not";
+            this.tbox1not.Size = new System.Drawing.Size(100, 23);
+            this.tbox1not.TabIndex = 2;
+            // 
+            // tbox2not
+            // 
+            this.tbox2not.Location = new System.Drawing.Point(110, 152);
+            this.tbox2not.Name = "tbox2not";
+            this.tbox2not.Size = new System.Drawing.Size(100, 23);
+            this.tbox2not.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Quicksand", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(53, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 28);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ad : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Quicksand", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(18, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 28);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Soyad : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Quicksand", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(36, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 28);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "1.Not :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Quicksand", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(28, 147);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 28);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "2.Not : ";
+            // 
+            // listboxad
+            // 
+            this.listboxad.FormattingEnabled = true;
+            this.listboxad.ItemHeight = 15;
+            this.listboxad.Location = new System.Drawing.Point(12, 254);
+            this.listboxad.Name = "listboxad";
+            this.listboxad.Size = new System.Drawing.Size(120, 109);
+            this.listboxad.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Quicksand", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(12, 223);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(36, 28);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Ad";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Quicksand", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(138, 223);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 28);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Soyad";
+            // 
+            // listboxsoyad
+            // 
+            this.listboxsoyad.FormattingEnabled = true;
+            this.listboxsoyad.ItemHeight = 15;
+            this.listboxsoyad.Location = new System.Drawing.Point(138, 254);
+            this.listboxsoyad.Name = "listboxsoyad";
+            this.listboxsoyad.Size = new System.Drawing.Size(120, 109);
+            this.listboxsoyad.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Quicksand", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(12, 365);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(58, 28);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "1.Not";
+            // 
+            // listboxnot1
+            // 
+            this.listboxnot1.FormattingEnabled = true;
+            this.listboxnot1.ItemHeight = 15;
+            this.listboxnot1.Location = new System.Drawing.Point(12, 396);
+            this.listboxnot1.Name = "listboxnot1";
+            this.listboxnot1.Size = new System.Drawing.Size(120, 109);
+            this.listboxnot1.TabIndex = 13;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Quicksand", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(138, 365);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(61, 28);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "2.Not";
+            // 
+            // listboxnot2
+            // 
+            this.listboxnot2.FormattingEnabled = true;
+            this.listboxnot2.ItemHeight = 15;
+            this.listboxnot2.Location = new System.Drawing.Point(138, 396);
+            this.listboxnot2.Name = "listboxnot2";
+            this.listboxnot2.Size = new System.Drawing.Size(120, 109);
+            this.listboxnot2.TabIndex = 15;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Quicksand", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point);
+            this.label9.Location = new System.Drawing.Point(12, 508);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(101, 28);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Ortalama";
+            // 
+            // ortalama
+            // 
+            this.ortalama.FormattingEnabled = true;
+            this.ortalama.ItemHeight = 15;
+            this.ortalama.Location = new System.Drawing.Point(12, 539);
+            this.ortalama.Name = "ortalama";
+            this.ortalama.Size = new System.Drawing.Size(120, 109);
+            this.ortalama.TabIndex = 17;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Quicksand", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point);
+            this.label10.Location = new System.Drawing.Point(138, 508);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(67, 28);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "Sonuç";
+            // 
+            // sonuc
+            // 
+            this.sonuc.FormattingEnabled = true;
+            this.sonuc.ItemHeight = 15;
+            this.sonuc.Location = new System.Drawing.Point(138, 539);
+            this.sonuc.Name = "sonuc";
+            this.sonuc.Size = new System.Drawing.Size(120, 109);
+            this.sonuc.TabIndex = 19;
+            // 
+            // hesapla
+            // 
+            this.hesapla.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.hesapla.Location = new System.Drawing.Point(116, 183);
+            this.hesapla.Name = "hesapla";
+            this.hesapla.Size = new System.Drawing.Size(101, 41);
+            this.hesapla.TabIndex = 21;
+            this.hesapla.Text = "HESAPLA";
+            this.hesapla.UseVisualStyleBackColor = true;
+            this.hesapla.Click += new System.EventHandler(this.hesapla_Click);
+            // 
+            // temizle
+            // 
+            this.temizle.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.temizle.Location = new System.Drawing.Point(12, 183);
+            this.temizle.Name = "temizle";
+            this.temizle.Size = new System.Drawing.Size(101, 41);
+            this.temizle.TabIndex = 22;
+            this.temizle.Text = "TEMİZLE";
+            this.temizle.UseVisualStyleBackColor = true;
+            this.temizle.Click += new System.EventHandler(this.temizle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(270, 657);
+            this.Controls.Add(this.temizle);
+            this.Controls.Add(this.hesapla);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.sonuc);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.ortalama);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.listboxnot2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.listboxnot1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.listboxsoyad);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.listboxad);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbox2not);
+            this.Controls.Add(this.tbox1not);
+            this.Controls.Add(this.tboxsoyad);
+            this.Controls.Add(this.tboxad);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tboxad;
+        private System.Windows.Forms.TextBox tboxsoyad;
+        private System.Windows.Forms.TextBox tbox1not;
+        private System.Windows.Forms.TextBox tbox2not;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listboxad;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox listboxsoyad;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ListBox listboxnot1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ListBox listboxnot2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ListBox ortalama;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.ListBox sonuc;
+        private System.Windows.Forms.Button hesapla;
+        private System.Windows.Forms.Button temizle;
+    }
+}
+
---
+++ Not Uygulaması/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Not_Uygulaması
+{
+    public partial class Form1 : Form
+    {
+        public int not1;
+        public int not2;
+        public int ort;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void hesapla_Click(object sender, EventArgs e)
+        {
+            not1 = Convert.ToInt32(tbox1not.Text);
+            not2 = Convert.ToInt32(tbox2not.Text);
+            ort = ((not1 + not2) / 2);
+            
+            listboxad.Items.Add("Adınız : " + tboxad.Text);
+            listboxsoyad.Items.Add("Soyadınız : " + tboxsoyad.Text);
+            listboxnot1.Items.Add("1. Notunuz : " + not1);
+            listboxnot2.Items.Add("2. Notunuz : " + not2);
+            ortalama.Items.Add("Ortalamanız : " + ort);
+            
+            if (ort >= 50)
+            {
+                sonuc.Items.Add("Geçtiniz.");
+            }
+            else
+            {
+                sonuc.Items.Add("Kaldınız.");
+            }
+
+            
+        }
+
+        private void temizle_Click(object sender, EventArgs e)
+        {
+            tboxad.Clear();
+            tboxsoyad.Clear();
+            tbox1not.Clear();
+            tbox2not.Clear();
+
+            listboxad.Items.Clear();
+            listboxsoyad.Items.Clear();
+            listboxnot1.Items.Clear();
+            listboxnot2.Items.Clear();
+            ortalama.Items.Clear();
+            sonuc.Items.Clear();
+        }
+    }
+}
